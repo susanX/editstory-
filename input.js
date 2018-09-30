f8258b22b20142ff21d2792d8800f117c0748f1e
susanX <susan.gooding@yahoo.co.uk>
2018-09-30 01:42:49 +0100

    input prompts

--- input.js
+++ input.js
@@ -1,1 +1,19 @@
-document.getElementById('one').innnerHTML = "More <em>sleep now</em>";
+document.getElementById('one').onclick = newList;
+
+var item1;
+var item2:
+var item3:
+
+function newList(){
+    
+    item1 = prompt("Enter a new fist item").innerHTML = item1;    
+    item2 = prompt("Enter a new fist item").innerHTML = item2; 
+    item3 = prompt("Enter a new fist item").innerHTML = item3; 
+    updateList();
+}
+
+function updateList() {
+    document.getElementById('one').innerHTML = item1;
+    document.getElementById('two').innerHTML = item2;
+    document.getElementById('three').innerHTML = item3;
+}

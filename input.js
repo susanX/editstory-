707848e81868f4f080afb0e9a9ee8b17b9a0cc8e
susanX <susan.gooding@yahoo.co.uk>
2018-09-30 11:30:32 +0100

    js edit prompt

--- input.js
+++ input.js
@@ -2,12 +2,12 @@ var item1;
 var item2:
 var item3:
 
-document.getElementById('changeList').onclick = newList;
+document.getElementById("changeList").onclick = newList;
 
 function newList(){   
     item1 = prompt("Enter a new fist item:");   
-    item2 = prompt("Enter a new fist item"); 
-    item3 = prompt("Enter a new fist item"); 
+    item2 = prompt("Enter a new second item"); 
+    item3 = prompt("Enter a new third item"); 
     updateList();
 }
 
